6b1f82764cd1b9ce4bf50377cc5d2f48536764a6
K-Pax <paksyfr@gmail.com>
2020-11-05 21:45:22 +0100

    VGG Dataset done

--- net_train_vgg.py
+++ net_train_vgg.py
@@ -1,18 +1,20 @@
 import torchvision.datasets as dset
+import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 import random
 import os
 
-from PIL import Image
-
+from PIL import Image, ImageOps
 
 
 vgg_data_path = os.path.join('D:\\', 'bigdata', 'vggface2_test', 'test')
 folder_dataset = dset.ImageFolder(root=vgg_data_path)
 
 class SiameseNetworkDatasetVGG(Dataset):
-    def __init__(self, folder):
+    def __init__(self, folder, transform=None, invert=True):
         self.folder = folder
+        self.transform = transform
+        self.invert = invert
 
     def __getitem__(self, index):
         img0_tuple = random.choice(self.folder.imgs)
@@ -25,12 +27,28 @@ class SiameseNetworkDatasetVGG(Dataset):
 
         img0 = Image.open(img0_tuple[0]).convert('L')
         img1 = Image.open(img1_tuple[0]).convert('L')
+
+        if self.invert:
+            img0 = ImageOps.invert(img0)
+            img1 = ImageOps.invert(img1)
+
+        if self.transform:
+            img0 = self.transform(img0)
+            img1 = self.transform(img1)
+
         return img0, img1
 
     def __len__(self):
         return len(self.folder.imgs)
 
 
-vgg_siamese_dataset = SiameseNetworkDatasetVGG(folder=folder_dataset)
+vgg_siamese_dataset = SiameseNetworkDatasetVGG(
+    folder=folder_dataset,
+    transform=transforms.Compose([
+        transforms.Resize((300, 300)),
+        transforms.ToTensor()
+    ]),
+    invert=True
+)
 
 
