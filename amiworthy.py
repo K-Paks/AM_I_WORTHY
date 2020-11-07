418bf2752cf1d6fe74ef485f35f0c2c5b1decccf
K-Pax <paksyfr@gmail.com>
2020-11-07 15:26:25 +0100

    New network connected, doesn't want to run on the GPU, though :(

--- amiworthy.py
+++ amiworthy.py
@@ -1,44 +1,59 @@
 import cv2
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
 from net import SiameseNetwork
 from utils import prep_face, FaceCropper
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = 'cpu'
+
 # load model
-model = SiameseNetwork().cuda()
-model.load_state_dict(torch.load('model.pth'))
+# model = SiameseNetwork().cuda()
+# model.load_state_dict(torch.load('model.pth'))
+model = models.resnet18().to(device)
+num_features = model.fc.in_features
+model.fc = nn.Linear(num_features, 5)
+model.load_state_dict(torch.load('model_vgg.pth'))
+
 faceCropper = FaceCropper(
     scaleFactor=1.3,
     minNeighbors=3,
-    minSize=(100, 100)
+    minSize=(100, 100),
+    face_size=(224, 224),
+    device=device
 )
 
-frameW = 640
-frameH = 480
-cap = cv2.VideoCapture(1)
+frameW = 1280
+frameH = 720
+cap = cv2.VideoCapture(0)
 cap.set(3, frameW)
 cap.set(4, frameH)
 
 # mePrep
-me_day = prep_face('me_day.jpg')
+me_day = prep_face('3day.jpg')
 # me_night = prep_face('me_night.jpg')
 
 
 while True:
     success, img = cap.read()
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # cv2.imwrite('3day.jpg', img_face)
 
-    face_data = faceCropper.get_face(img_gray)
+    face_data = faceCropper.get_face(img)
     if face_data is not None:
         timg, x, y, w, h = face_data
-        x1, x2 = model(timg, me_day)
+        x1 = model(timg)
+        x2 = model(me_day)
+        # x1, x2 = model(timg, me_day)
         euc_dist = F.pairwise_distance(x1, x2)
+        print(euc_dist.item())
 
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 3)
-        if euc_dist.item() < 2.7:
+        if euc_dist.item() < 0.5:
             cv2.putText(img, "Karol", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 1)
         else:
             print(euc_dist.item())
--- utils.py
+++ utils.py
@@ -3,24 +3,27 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # `amiworthy.py` helpers
 
 def prep_face(file):
     face = cv2.imread(file)
     face = cv2.resize(face, (100, 100))
-    face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    # face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
     # cv2.imwrite('1_2.jpg', face)
     face = face.astype(np.float32)
-    face = torch.from_numpy(face).reshape(1, 1, 100, 100)
-    return face.cuda()
+    face = torch.from_numpy(face).reshape(1, 3, 100, 100)
+    return face
 
 
 class FaceCropper():
-    def __init__(self, scaleFactor, minNeighbors, minSize):
+    def __init__(self, scaleFactor, minNeighbors, minSize, face_size, device):
         self.faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
         self.scaleFactor = scaleFactor
         self.minNeighbors = minNeighbors
         self.minSize = minSize
+        self.face_size = face_size
+        self.device = device
 
     def get_face(self, img):
         faces = self.faceCascade.detectMultiScale(
@@ -32,27 +35,29 @@ class FaceCropper():
 
         for (x, y, w, h) in faces:
             img_face = img[y:y + h, x:x + w]
-            img_face = cv2.resize(img_face, (100, 100))
+            img_face = cv2.resize(img_face, self.face_size)
             npimg = img_face.astype(np.float32) / 255
-            timg = torch.from_numpy(npimg).reshape(1, 1, 100, 100)
-            timg = timg.cuda()
+            timg = torch.from_numpy(npimg).reshape((1, 3) + self.face_size)
+            timg = timg.to(self.device)
             return timg, x, y, w, h
 
 
 # `net_train.py` helpers
 
-def imshow(img,text=None,should_save=False):
+def imshow(img, text=None, should_save=False):
     npimg = img.numpy()
     plt.axis("off")
     if text:
-        plt.text(75, 8, text, style='italic',fontweight='bold',
-            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+        plt.text(75, 8, text, style='italic', fontweight='bold',
+                 bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 10})
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
-def show_plot(iteration,loss):
-    plt.plot(iteration,loss)
+
+def show_plot(iteration, loss):
+    plt.plot(iteration, loss)
     plt.show()
+
 
 def cuda_maybe(device, *args):
     return [arg.to(device) for arg in args]
